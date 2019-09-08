1829cd310a803e9633f06e556b29c369c890a10a
Eric Wallace ericwallace@berkeley.edu <ericwallace@pinwheel3.d2.comp.nus.edu.sg>
2019-09-09 02:10:46 +0800

    upd

--- eric_scripts/4-download_data_and_models.sh
+++ eric_scripts/4-download_data_and_models.sh
@@ -1,5 +1,10 @@
-mkdir -p probing_data
-/home/ericwallace/bin/rclone copy -v Eric:finetuning/probing_data/edges/ontonotes/const/ probing_data/edges/ontonotes/const/
+#mkdir -p probing_data
+#/home/ericwallace/bin/rclone copy -v Eric:finetuning/probing_data/edges/ontonotes/const/ probing_data/edges/ontonotes/const/
 mkdir -p models
+mkdir -p models/5-way-multiqa
+/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/5-way-multiqa models/5-way-multiqa
+mkdir -p models/SST
+/home/ericwallace/bin/rclone copy -v Eric:finetuning/models/SST models/SST
 mkdir -p models/MNLI
 /home/ericwallace/bin/rclone copy -v Eric:finetuning/models/MNLI models/MNLI
+
